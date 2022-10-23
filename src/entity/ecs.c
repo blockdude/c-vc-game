17444046d7453289d959d5ae4ed15207b96a3624
Anthony Cobb <2blockdude@gmail.com>
2022-10-22 22:35:57 -0400

    trying I guess

--- src/entity/ecs.c
+++ src/entity/ecs.c
@@ -1,6 +1,50 @@
 #include "ecs.h"
-#include "../data/vector.h"
+#include <data/vector.h>
 
 #define ECS_INIT_COMPONENT( name ) \
     extern int ecs_##name##_init( struct ecs * ); \
     ecs_##name##_init( self );
+
+#define ECS_INIT_CAPACITY 256
+
+int ecs_init( struct ecs *self )
+{
+    // init vectors
+    self->component = vector_new( sizeof( *self->component ), ECS_INIT_CAPACITY );
+    self->system = vector_new( sizeof( *self->system ), ECS_INIT_CAPACITY );
+    self->entity = vector_new( sizeof( *self->entity ), ECS_INIT_CAPACITY );
+
+    return 0;
+}
+
+int ecs_free( struct ecs *self )
+{
+    for ( size_t i = 0; i < vector_size( self->component ); i++ )
+        vector_free( self->component[ i ] );
+    for ( size_t i = 0; i < vector_size( self->system ); i++ )
+        vector_free( self->system[ i ] );
+    vector_free( self->entity );
+
+    return 0;
+}
+
+ecs_system_t ecs_register_system( struct ecs *self, ecs_system_fn fn, ecs_event_t event )
+{
+    // make sure the event exists
+    if ( event >= vector_size( self->system ) )
+        return ECS_REGISTER_ERROR;
+
+    // add fn to vector
+    self->system[ event ] = vector_push_back( self->system[ event ], NULL );
+    size_t back = vector_size( self->system[ event ] ) - 1;
+    self->system[ event ][ back ] = fn;
+
+    // create system id
+    ecs_system_t sys_id;
+    sys_id = event << 15;
+    sys_id += back;
+
+    return sys_id;
+}
+
+ecs_component_t ecs_register_component( struct ecs *self, size_t component_size );
--- src/entity/ecs.h
+++ src/entity/ecs.h
@@ -1,7 +1,7 @@
 #ifndef ECS_H
 #define ECS_H
 
-#include "../util/util.h"
+#include <util/util.h>
 
 /*
  * defines
@@ -24,66 +24,19 @@ typedef ecs_id_t ecs_system_t;
 typedef ecs_id_t ecs_entity_t;
 
 /*
- * component stuff
- */
-
-struct ecs_component_data
-{
-    /* array of individual components */
-    void *data;
-    size_t size;
-};
-
-/*
- * system stuff
- */
-
-struct ecs_system_data
-{
-    ecs_system_fn fn;
-    ecs_component_t *components;
-    size_t component_count;
-};
-
-/*
- * lists
- */
-
-struct ecs_system_list
-{
-    struct ecs_system_data *list;
-    size_t count;
-    size_t max;
-};
-
-struct ecs_event_list
-{
-    struct ecs_system_list *system;
-    size_t count;
-    size_t max;
-};
-
-struct ecs_component_list
-{
-    struct ecs_component_data *list;
-    size_t count;
-    size_t max;
-};
-
-struct ecs_entity_list
-{
-    ecs_entity_t *list;
-    size_t count;
-    size_t max;
-};
-
-/*
  * ecs stuff
  */
 
 struct ecs
 {
-    int tmp;
+    // vector of component pools
+    void **component;
+
+    // 2d vector of systems (1 dimension for events and 1 for the systems)
+    ecs_system_fn **system;
+
+    // vector of entity ids
+    ecs_entity_t *entity;
 };
 
 /*
@@ -94,24 +47,24 @@ struct ecs
 int ecs_init( struct ecs *self );
 int ecs_free( struct ecs *self );
 
+// calling events
+ecs_event_t ecs_event_create( struct ecs *self );
+int ecs_event_delete( struct ecs *self, ecs_event_t event );
+int ecs_call_event( struct ecs *self, ecs_event_t event );
+
+// register stuff which return an id to its respective call
+ecs_system_t ecs_register_system( struct ecs *self, ecs_system_fn fn, ecs_event_t event );
+ecs_component_t ecs_register_component( struct ecs *self, size_t component_size );
+
 void *ecs_get_components( struct ecs *self, ecs_component_t component, size_t *count );
 
 // entity creation
-ecs_entity_t ecs_entity_new( struct ecs *self );
-int ecs_entity_del( struct ecs *self, ecs_entity_t entity );
+ecs_entity_t ecs_entity_create( struct ecs *self );
+int ecs_entity_delete( struct ecs *self, ecs_entity_t entity );
 
 // component creation for entities
 int ecs_entity_add_component( struct ecs *self, ecs_entity_t entity, ecs_component_t component, void *data );
 int ecs_entity_del_component( struct ecs *self, ecs_entity_t entity, ecs_component_t component );
 void *ecs_entity_get_component( struct ecs *self, ecs_entity_t entity, ecs_component_t component );
-
-// calling events
-ecs_event_t ecs_event_new( struct ecs *self );
-int ecs_event_del( struct ecs *self, ecs_event_t event );
-int ecs_call_event( struct ecs *self, ecs_event_t event );
-
-// register stuff which return an id to its respective call
-ecs_system_t ecs_register_system( struct ecs *self, ecs_system_fn fn, ecs_event_t event, size_t component_count, ecs_component_t *components );
-ecs_component_t ecs_register_component( struct ecs *self, size_t component_size );
 
 #endif
