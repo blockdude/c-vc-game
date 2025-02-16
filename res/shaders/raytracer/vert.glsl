d07b26d0171e3b733bc03f9adfc00937dc4872cb
juliet <juliet@JULIET>
2025-02-16 16:39:25 -0500

    fdsjafjjjjjjj

--- res/shaders/raytracer/vert.glsl
+++
@@ -1,9 +0,0 @@
-#version 330 core
-
-in vec3 position;
-in vec3 normal;
-
-void main()
-{
-	gl_Position = vec4( position.xyz, 1.0f );
-}
--- scripts/run.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-export LD_LIBRARY_PATH=bld/bin/sdl:bld/bin/lib:bld/bin/game:$LD_LIBRARY_PATH
-mangohud --dlsym $1
